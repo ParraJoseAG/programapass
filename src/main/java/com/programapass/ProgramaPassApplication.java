1daba9297aed4672f0e0af713278d2f2772f094f
JOSE GUILLERMO PARRA APONTE <parrajoseag@gmail.com>
2020-11-18 05:13:42 -0300

    Initial commit

---
+++ src/main/java/com/programapass/ProgramaPassApplication.java
@@ -0,0 +1,13 @@
+package com.programapass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramaPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramaPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programapass/ProgramaPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programapass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramaPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
